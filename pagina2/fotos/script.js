9e18dd47b5790023cbb8079634ee85c4d850e367
Gildevan Sousa <gildevan87654321@gmail.com>
2022-04-11 17:11:56 -0300

    add da pagina 2com fotos

---
+++ pagina2/fotos/script.js
@@ -0,0 +1,27 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora < 12){
+        // BOM DIA!
+        img.src = 'foto/fotomanhã.png'
+    }
+    
+    else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+        img.src = 'foto/fototarde.png'
+    }
+    
+    else {
+        // BOA NOITE!
+        img.src = 'fotos/fotonoite.png'
+    }
+
+
+}
+
+ 
